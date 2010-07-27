950d633f5862ce080279f7bcf49bf8a4da5cbd64
A.I. McLeod <aimcleod@uwo.ca>
2010-07-27 00:00:00 +0000

    version 1.51

--- R/savews.R
+++ R/savews.R
@@ -2,63 +2,69 @@ savews <-
 function (name = .WSID, d = as.character(.UserDate), silentQ = FALSE, 
     historyQ = TRUE, q = FALSE, prefix=.Prefix) 
 {
-    if(!testrwm()) 
-       stop("`.UserDirectory` and/or `.UserDate` not set correctly. See help(rwm).")
-    if (!exists(".WSID", where=1, inherits=FALSE) && !is.character(name)) 
-        stop("`.WSID` not defined!\nArgument name must be character string if used.\nOtherwise if argument `name` is not used, `.WSID` must be a valid pathanme with read/write privledges.")
-    if (missing(prefix)&&!exists(".Prefix", where=1, inherits=FALSE) ) 
-        Prefix<-""
-    else
-        Prefix<-prefix
-    integerQ <- function(n) ceiling(n)==floor(n)
-    if (is.numeric(Prefix) && (Prefix>=0) && integerQ(Prefix))
-        Prefix <- as.character(Prefix)        
-#test if 'prefix' is not a character string, eg: myws instead of "myws"
-    ans <- base::try(is.character(Prefix),  silent=TRUE) 
-    if (!(is.logical(ans) && ans))
-        base::stop("Argument, 'prefix', must be a character string!")
     `%<>%` <- function(x,y) paste(x,y,sep="")  
-#test if .UserDirectory<>d is valid
-    FullUserDirectory <- if (d=="")
-        .UserDirectory
-    else
-       .UserDirectory %<>% "/" %<>% d
-    if (!file.exists(FullUserDirectory)) {
-        dir.create(FullUserDirectory, recursive=TRUE)
-        if (file.exists(FullUserDirectory))
-        cat(FullUserDirectory %<>% " created!", fill = TRUE)
-      else stop("error creating" %<>% FullUserDirectory)
-    }
-#name must be a character string or missing
-    ans <- try(is.character(name),  silent=TRUE)
-    if (!(is.logical(ans) && ans))
-        stop("First argument must be a character string")
-    is.same.name <- function(x, y) is.logical(all.equal(x, y)) && 
-        all.equal(x, y)
-#case: name=.WSID
-    if (exists(".WSID", where=1, inherits=FALSE) && is.same.name(name, .WSID)) 
-        WSPathName <- name
-#case: if .WSID exists and `name` specifies same workspace
-    else if (exists(".WSID", where=1, inherits=FALSE)) {
-        WSPathName <- if (d=="")
-            .UserDirectory %<>% "/" %<>% name
+    if (file.exists(name)){
+        WSPathName<-name
+        Prefix <- ""
+        }
+    else{
+        if(!testrwm()) 
+            stop("`.UserDirectory` and/or `.UserDate` not set correctly. See help(rwm).")
+        if (!exists(".WSID", where=1, inherits=FALSE) && !is.character(name)) 
+            stop("`.WSID` not defined!\nArgument name must be character string if used.\nOtherwise if argument `name` is not used, `.WSID` must be a valid pathanme with read/write privledges.")
+        if (missing(prefix)&&!exists(".Prefix", where=1, inherits=FALSE) ) 
+            Prefix<-""
         else
-            .UserDirectory %<>% "/" %<>% d %<>% "/" %<>% name
+            Prefix<-prefix
+        integerQ <- function(n) ceiling(n)==floor(n)
+        if (is.numeric(Prefix) && (Prefix>=0) && integerQ(Prefix))
+            Prefix <- as.character(Prefix)        
+#test if 'prefix' is not a character string, eg: myws instead of "myws"
+        ans <- base::try(is.character(Prefix),  silent=TRUE) 
+        if (!(is.logical(ans) && ans))
+            base::stop("Argument, 'prefix', must be a character string!")
+#test if .UserDirectory<>d is valid
+        if (d=="")
+            FullUserDirectory <- .UserDirectory
+        else
+            FullUserDirectory <- .UserDirectory %<>% "/" %<>% d
+        if (!file.exists(FullUserDirectory)) {
+            dir.create(FullUserDirectory, recursive=TRUE)
+        if (file.exists(FullUserDirectory))
+            cat(FullUserDirectory %<>% " created!", fill = TRUE)
+        else stop("error creating" %<>% FullUserDirectory)
+        }
+#name must be a character string or missing
+        ans <- try(is.character(name),  silent=TRUE)
+        if (!(is.logical(ans) && ans))
+            stop("First argument must be a character string")
+        is.same.name <- function(x, y) is.logical(all.equal(x, y)) && 
+            all.equal(x, y)
+#case: name=.WSID
+        if (exists(".WSID", where=1, inherits=FALSE) && is.same.name(name, .WSID)) 
+            WSPathName <- name
+#case: if .WSID exists and `name` specifies same workspace
+        else if (exists(".WSID", where=1, inherits=FALSE)) {
+            WSPathName <- if (d=="")
+                .UserDirectory %<>% "/" %<>% name
+            else
+                .UserDirectory %<>% "/" %<>% d %<>% "/" %<>% name
 #workspace name has changed, check if directory exists
-        if (!file.exists(WSPathName)) {
-            dir.create(WSPathName, recursive=TRUE)
-            if (file.exists(WSPathName)) cat(WSPathName  %<>%  " created!", fill = TRUE)
-            else stop(paste("error creating", WSPathName))
-        }         
-    }
-    else {#.WSID does not exist
-        WSPathName<-paste(.UserDirectory, d, name, sep="/")
-         if (!file.exists(WSPathName)) {
-            dir.create(WSPathName, recursive=TRUE)
-            if (file.exists(WSPathName)) 
-                cat(WSPathName  %<>%  " created!", fill = TRUE)
-            else stop("error creating "  %<>%  WSPathName)
-        } 
+            if (!file.exists(WSPathName)) {
+                dir.create(WSPathName, recursive=TRUE)
+                if (file.exists(WSPathName)) cat(WSPathName  %<>%  " created!", fill = TRUE)
+                else stop(paste("error creating", WSPathName))
+            }         
+        }
+        else {#.WSID does not exist
+            WSPathName<-paste(.UserDirectory, d, name, sep="/")
+            if (!file.exists(WSPathName)) {
+                dir.create(WSPathName, recursive=TRUE)
+                if (file.exists(WSPathName)) 
+                    cat(WSPathName  %<>%  " created!", fill = TRUE)
+                else stop("error creating "  %<>%  WSPathName)
+            } 
+        }
     }
     base::assign(".WSID", WSPathName, envir = .GlobalEnv)
     if (Prefix!="")
